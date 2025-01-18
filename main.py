e06f1a44a78f4ca881e8559b80d5450699143662
1nazar1l <116957441+1nazar1l@users.noreply.github.com>
2025-01-18 23:22:11 +0300

    Rename values and make code more clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import datetime
 
 import os
 
@@ -13,7 +12,6 @@ def get_correct_number(number, range=True):
                  continue
             else:
                 number = number.replace(el, "")
-    
     return number
 
 def is_range(number):
@@ -26,59 +24,60 @@ def is_range(number):
 
     return err == 0
 
-def breaking_range(item, chapters):
+def breaking_range(item, all_chapters):
     chapters_range = item.split(':')
     min, max = chapters_range
     min, max = int(min), int(max)
     if min > max:
         min, max = max, min
-    for i in range(min, max+1):
-        chapters.append(i)
+    for chapter_number in range(min, max+1):
+        all_chapters.append(chapter_number)
 
-def get_correct_sorted_chapters(user_string, chapters):
-    splited_string = user_string.split(',')
-    for num, number in enumerate(splited_string):
-        number = get_correct_number(number, is_range(number))
-        splited_string[num] = number
-        if number == "":
-            splited_string.remove("")
-        elif number == " ":
-            splited_string.remove(" ")
+def get_correct_sorted_chapters(user_chapters, all_chapters):
+    splited_user_chapters = user_chapters.split(',')
+    for ordinal, chapter_number in enumerate(splited_user_chapters):
+        chapter_number = get_correct_number(chapter_number, is_range(chapter_number))
+        splited_user_chapters[ordinal] = chapter_number
+
+    for chapter_number in splited_user_chapters:
+        if chapter_number == "":
+            splited_user_chapters.remove("")
+        elif chapter_number == " ":
+            splited_user_chapters.remove(" ")
      
-    for item in splited_string:
-        if ":" in item:
-            breaking_range(item, chapters)
+    for chapter_number in splited_user_chapters:
+        if ":" in chapter_number:
+            breaking_range(chapter_number, all_chapters)
         else: 
-            item = int(item)
-            chapters.append(item)
+            chapter_number = int(chapter_number)
+            all_chapters.append(chapter_number)
 
-    sorted_chapters = sorted(list(set(chapters)))
+    sorted_chapters = sorted(list(set(all_chapters)))
     if sorted_chapters[0] == 0:
         sorted_chapters.remove(0)
 
     return sorted_chapters
 
-def find_manga_on_page(url, text_to_find, manga):
-    i = 9
+def find_manga_on_page(manga_search_url, text_to_find):
+    i = 0
     found = False
 
     while not found:
         i += 1
         params = {"page": i}
-        response = requests.get(url, params=params)
+        response = requests.get(manga_search_url, params=params)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'html.parser')
         selector = "main div.grid-cols-2 .manga-card a h2"
         cards = soup.select(selector)
-        
         for card in cards:
             if text_to_find in card.text:  
                 a = card.parent
-                href = a["href"].split("/")[-1]
-                manga.append(card.text)
-                manga.append(href)
+                manga_slug = a["href"].split("/")[-1]
                 found = True  
                 break  
+    
+    return manga_slug
 
 def find_last_chapter(url):
     response = requests.get(url)
@@ -88,41 +87,12 @@ def find_last_chapter(url):
     last_chapter = href.split("/")[-1].split("-")[-1]
     return int(last_chapter)
 
-def main():
-    user_string = "1:12"
-    # user_string = input("Введите значения: ")
-    chapters = []
-    sorted_chapters_counter = get_correct_sorted_chapters(user_string, chapters)
-    if len(sorted_chapters_counter) == 0:
-        print("Перепроверьте главы которые вы написали для скачивания.")
-    # print(sorted_chapters)
-
-    current_time1 = datetime.datetime.now().time()
-    # print(current_time1) 
-    manga = []
-    url = "https://mangapoisk.live/manga"
-    text_to_find = "Жрец порчи" 
-    find_manga_on_page(url, text_to_find, manga)  
-    current_time2 = datetime.datetime.now().time()
-    # print(current_time2) 
-
-    url = f"https://mangapoisk.live/manga/{manga[1]}"
-    last_chapter = find_last_chapter(url)
-    # print(last_chapter)
-
-    suitable_chapters_counter = []
-    for number in sorted_chapters_counter:
-        if number <= last_chapter:
-            suitable_chapters_counter.append(number)
-    # print(suitable_chapters_counter)
-    z = 1
+def download_images(numbers_relevant_chapters, manga_slug):
     os.makedirs("imgs", exist_ok=True)
-    for i in suitable_chapters_counter:
-        if z == 3:
-            break
+    for i in numbers_relevant_chapters:
         page = f"imgs/page{i}"
         os.makedirs(page, exist_ok=True)
-        url = f"https://mangapoisk.live/manga/{manga[1]}/chapter/1-{i}"
+        url = f"https://mangapoisk.live/manga/{manga_slug}/chapter/1-{i}"
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -139,8 +109,34 @@ def main():
 
                 with open(filepath, 'wb') as f:
                     f.write(response.content)
-                # print(i["src"])
-        z +=1
+
+
+def main():
+    user_chapters = "3jsdfn.,1:9,0, ,:1:1ddad.,1sdf2."
+    all_chapters = []
+    numbers_sorted_chapters = get_correct_sorted_chapters(user_chapters, all_chapters)
+    if len(numbers_sorted_chapters) == 0:
+        print("Перепроверьте главы которые вы написали для скачивания.")
+    print(numbers_sorted_chapters)
+
+    # manga_page_url = input("Введите ссылку на страницу с мангой.")
+    # manga_page_url = "https://mangapoisk.live/manga/the-reincarnated-assassin-is-a-genius-swordsman"
+    # if manga_page_url:
+    #     manga_slug = manga_page_url.split("/")[-1]
+    # else:
+    #     manga_search_url = "https://mangapoisk.live/manga"
+    #     text_to_find = "Жрец порчи" 
+    #     manga_slug = find_manga_on_page(manga_search_url, text_to_find)  
+    #     manga_page_url = f"https://mangapoisk.live/manga/{manga_slug}"
+
+    # last_chapter = find_last_chapter(manga_page_url)
+
+    # numbers_relevant_chapters = []
+    # for chapter_number in numbers_sorted_chapters:
+    #     if chapter_number <= last_chapter:
+    #         numbers_relevant_chapters.append(chapter_number)
+
+    # download_images(numbers_relevant_chapters, manga_slug)
 
 if __name__ == "__main__":
     main()
